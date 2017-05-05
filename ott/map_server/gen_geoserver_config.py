1dd778a2680c084f474f9807a32fbed8df8d3942
Frank Purcell <fxpurcell@gmail.com>
2017-05-05 09:59:33 -0700

    more work on feeds

--- ott/map_server/gen_geoserver_config.py
+++ ott/map_server/gen_geoserver_config.py
@@ -1,4 +1,6 @@
 from mustache import template
+from ott.utils import gtfs_utils
+
 
 template_dir = 'ott/map_server/templates/'
 
@@ -12,4 +14,8 @@ def stores(db_name='ott', schema='TRIMET', user='geoserve'):
     return v
 
 def generate_all():
-    print stores()
+    feed_list = gtfs_utils.get_feeds_from_config()
+    for feed in feed_list:
+        feed_name = gtfs_utils.get_schema_name_from_feed(feed)
+        print feed_name
+        #print stores()
