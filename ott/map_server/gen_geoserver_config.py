b0e750b323d7cd054eb8c274aacae7058541bf06
Frank Purcell <fxpurcell@gmail.com>
2017-05-05 12:20:44 -0700

    Mo Wrrky

--- ott/map_server/gen_geoserver_config.py
+++ ott/map_server/gen_geoserver_config.py
@@ -1,5 +1,7 @@
+import os
 from mustache import template
 from ott.utils import gtfs_utils
+from ott.utils import file_utils
 
 
 template_dir = 'ott/map_server/templates/'
@@ -13,8 +15,15 @@ def stores(db_name='ott', schema='TRIMET', user='geoserve'):
     }
     return v
 
-def generate_all():
+def generate_all(geo_workspace="geoserver/data/workspaces/ott"):
     feed_list = gtfs_utils.get_feeds_from_config()
     for feed in feed_list:
         feed_name = gtfs_utils.get_schema_name_from_feed(feed)
-        print stores(schema=feed_name)
+        dir_path = os.path.join(geo_workspace, feed_name)
+        file_path = os.path.join(dir_path, 'datastore.xml')
+        file_utils.mkdir(dir_path)
+
+        with open(file_path, 'w+') as f:
+            content = stores(schema=feed_name)
+            f.write(content)
+
